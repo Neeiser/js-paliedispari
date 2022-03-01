01d6f15c37dc14ccb8e7adbde2bfffd53dab75b0
Neeiser <stortini.study@gmail.com>
2022-03-01 23:30:37 +0100

    Pari o dispari: Random function.

--- js/script.js
+++ js/script.js
@@ -1,26 +1,18 @@
-/* ---- Palindroma ---- */
 
-/* - Chiedere all’utente di inserire una parola */
-/* - Creare una funzione per capire se la parola inserita è palindroma */
-
-/* ---- Pari e Dispari ---- */
-
-/* - L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. */
-/* - Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). */
-/* - Sommiamo i due numeri */
-/* - Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) */
-/* - Dichiariamo chi ha vinto. */
-
-
-
-/* Palindroma */
+/*
+-------------------------------------------------------
+Palindroma
+------------------------------------------------------- 
+- Chiedere all’utente di inserire una parola
+- Creare una funzione per capire se la parola inserita è palindroma 
+*/
 
 const palindromoInput = document.getElementById('palindromo-input');
 const palindromoBtn = document.getElementById('palindromo-btn');
 const palindromoResult = document.getElementById('palindromo-risultato');
 
 function palindromoFase1(parola) {
-    
+
     let parolaReversed = parola.split('').reverse().join('');
     let validator;
 
@@ -40,3 +32,20 @@ palindromoBtn.addEventListener('click', function(){
         palindromoResult.innerHTML = 'La parola non è palindroma'
     }
 })
+
+
+/*
+-------------------------------------------------------
+Pari o dispari
+------------------------------------------------------- 
+- L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+- Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+- Sommiamo i due numeri
+- Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+- Dichiariamo chi ha vinto. 
+*/
+
+function random(min, max) {
+    numeroRandom = Math.floor(Math.random() * (max - min) ) + min;
+    return numeroRandom;
+};
